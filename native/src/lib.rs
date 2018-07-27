9e3f0d865bf87991d719fc6376e7b796624d2d49
samrg472 <samrg472@gmail.com>
2018-07-27 12:45:34 -0700

    Initialize the library when loading

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,10 +1,19 @@
 #[macro_use] extern crate neon;
 extern crate godcoin;
 
+use neon::prelude::*;
+
 mod asset;
 use asset::*;
 
+fn init(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    if godcoin::init().is_ok() { Ok(JsUndefined::new()) }
+    else { cx.throw_error("failed to initialize library") }
+}
+
 register_module!(mut cx, {
+    cx.export_function("init", init)?;
+
     cx.export_class::<JsAsset>("Asset")?;
     cx.export_function("Asset_from_string", asset_from_string)?;
 
